9b6175d103934f31288db6aaa16b10c9edc4ed4c
Seven <shiweidu@outlook.com>
2021-08-24 05:55:56 +0800

    *

--- client-api/src/app.module.ts
+++ client-api/src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    GraphQLModule.forRoot({
+      path: '/',
+      playground: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
