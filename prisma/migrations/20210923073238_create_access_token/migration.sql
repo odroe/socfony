7dc244d8e1ef7cb32f6188a4505096619c1ac4ba
Seven Du <shiweidu@outlook.com>
2021-09-23 15:34:16 +0800

    chore: create user profile schema

--- prisma/migrations/20210923073238_create_access_token/migration.sql
+++ prisma/migrations/20210923073238_create_access_token/migration.sql
@@ -9,6 +9,3 @@ CREATE TABLE `AccessToken` (
     INDEX `AccessToken_userId_idx`(`userId`),
     PRIMARY KEY (`token`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- AddForeignKey
-ALTER TABLE `AccessToken` ADD CONSTRAINT `AccessToken_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
