2a0aeb154605eaa1dc95a4e0fa617b58b6439029
Seven Du <shiweidu@outlook.com>
2021-12-03 19:25:02 +0800

    WIP

--- server/src/user-profile/user-profile.service.ts
+++ server/src/user-profile/user-profile.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Gender, PrismaClient, User, UserProfile } from '@prisma/client';
+import { UserProfile_gender, PrismaClient, User, UserProfile } from '@prisma/client';
 import { UserProfileEntity } from 'src/protobuf/socfony_pb';
 
 @Injectable()
@@ -28,7 +28,7 @@ export class UserProfileService {
 
   // Resolve profile gender to GRPC gender type.
   gender2grpcGender(
-    gender: Gender,
+    gender: UserProfile_gender,
   ): UserProfileEntity.GenderMap[keyof UserProfileEntity.GenderMap] {
     return UserProfileEntity.Gender[gender.toUpperCase()];
   }
@@ -36,14 +36,14 @@ export class UserProfileService {
   // GRPC gender tranform to Prisma gender
   grpcGender2gender(
     gender: UserProfileEntity.GenderMap[keyof UserProfileEntity.GenderMap],
-  ): Gender {
+  ): UserProfile_gender {
     switch (gender) {
       case UserProfileEntity.Gender.WOMAN:
-        return Gender.woman;
+        return UserProfile_gender.woman;
       case UserProfileEntity.Gender.MAN:
-        return Gender.man;
+        return UserProfile_gender.man;
     }
 
-    return Gender.unknown;
+    return UserProfile_gender.unknown;
   }
 }
