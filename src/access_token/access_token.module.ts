f8be9359035d248d6a5f24b3496349eb740316aa
Seven Du <whine.whitish.0u@icloud.com>
2021-12-08 19:29:05 +0800

    chore(WIP): 完成 Access Token 相关

--- src/access_token/access_token.module.ts
+++ src/access_token/access_token.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { VerificationCodeModule } from 'verification_code/verification_code.module';
 import { AccessTokenResolver } from './access_token.resolver';
+import { AccessTokenService } from './access_token.service';
 
 @Module({
-  providers: [AccessTokenResolver],
+  imports: [VerificationCodeModule],
+  providers: [AccessTokenResolver, AccessTokenService],
+  exports: [AccessTokenService],
 })
 export class AccessTokenModule {}
--- src/access_token/access_token.resolver.ts
+++ src/access_token/access_token.resolver.ts
@@ -1,13 +1,58 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { parsePhoneNumber } from 'libphonenumber-js';
+import { Auth } from 'shared/auth/auth.decorator';
+import { VerificationCodeService } from 'verification_code/verification_code.service';
+import { AccessTokenService } from './access_token.service';
 import { CreateAccessTokenArgs } from './dto/create_access_token.args';
 import { AccessToken } from './entities/access_token.entity';
 
 @Resolver(() => AccessToken)
 export class AccessTokenResolver {
+  constructor(
+    private readonly accessTokenService: AccessTokenService,
+    private readonly verificationCodeService: VerificationCodeService,
+  ) {}
+
   @Mutation(() => AccessToken)
   async createAccessToken(
     @Args({ type: () => CreateAccessTokenArgs }) args: CreateAccessTokenArgs,
   ) {
-    console.log(args);
+    const { phone, otp } = args;
+    const validatedPhone = this.validatePhone(phone);
+    const done = await this.validateOtp(validatedPhone, otp);
+    const result = await this.accessTokenService.authRegister(validatedPhone);
+
+    done();
+
+    return result;
+  }
+
+  @Mutation(() => AccessToken)
+  @Auth.refresh()
+  async refreshAccessToken(@Auth.accessToken() accessToken: AccessToken) {
+    return this.accessTokenService.refresh(accessToken);
+  }
+
+  @Mutation(() => Boolean)
+  @Auth.refresh()
+  async revokeAccessToken(@Auth.accessToken() accessToken: AccessToken) {
+    this.accessTokenService.delete(accessToken);
+
+    return true;
+  }
+
+  private validatePhone(phone: string): string {
+    return parsePhoneNumber(phone).format('E.164');
+  }
+
+  private async validateOtp(phone: string, otp: string): Promise<() => void> {
+    const isValid = await this.verificationCodeService.verify(phone, otp);
+    if (!isValid) {
+      throw new Error('Invalid OTP');
+    }
+
+    return () => {
+      this.verificationCodeService.delete(phone, otp);
+    };
   }
 }
