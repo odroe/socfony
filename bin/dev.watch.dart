733f7e0ced428793630f96c767848eb75a3487ca
Seven Du <whine.whitish.0u@icloud.com>
2021-12-17 13:55:20 +0800

    Update dev.watch.dart

--- bin/dev.watch.dart
+++ bin/dev.watch.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2021, Odroe Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 import 'package:server/app.dart';
 import 'package:watcher/watcher.dart';
 
@@ -6,7 +10,7 @@ void main() {
   final Watcher watcher = Watcher('lib');
   watcher.events.listen((WatchEvent event) async {
     print('${event.type} ${event.path}');
-    await app.shutdown();
+    await app.close();
     app.run();
   });
 
