a8e0f46724d582b6cd479269e91748da2b66c45b
Seven Du <whine.whitish.0u@icloud.com>
2021-12-17 22:57:38 +0800

    Update user.service.dart

--- lib/services/user.service.dart
+++ lib/services/user.service.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2021, Odroe Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 import 'package:grpc/grpc.dart';
 import 'package:server/database/connection_pool.dart';
 import 'package:server/protos/google/protobuf/timestamp.pb.dart';
