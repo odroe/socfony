61d8289e7307d9b31ca4e1d4946cca7a91e381a8
Seven Du <whine.whitish.0u@icloud.com>
2022-02-18 19:01:55 +0800

    Update graphql.module.ts

--- src/graphql.module.ts
+++ src/graphql.module.ts
@@ -2,12 +2,15 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule as $ } from '@nestjs/graphql';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
-export const GraphQLModule = $.forRoot({
+export const GraphQLModule = $.forRoot<ApolloDriverConfig>({
+  driver: ApolloDriver,
   autoSchemaFile: true,
   playground: false,
   path: '/graphql',
+  sortSchema: true,
   plugins: [ApolloServerPluginLandingPageLocalDefault()],
 });
