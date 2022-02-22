96745f167dfa02bd97f733b4c0001e90cad6fd56
Seven Du <whine.whitish.0u@icloud.com>
2022-02-22 11:45:11 +0800

    chore: add LICENSE

--- src/auth/auth-nullable.guard.ts
+++ src/auth/auth-nullable.guard.ts
@@ -1,3 +1,7 @@
+// Copyright (c) 2021, Odroe Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
 import { AccessToken, PrismaClient } from '@prisma/client';
