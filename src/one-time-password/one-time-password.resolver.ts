ee289a26e5da5ddb121b1cbc6261c5cecab6c2a7
Seven Du <whine.whitish.0u@icloud.com>
2022-03-01 16:17:53 +0800

    chore: Merge send Phone/E-Mail mutation to one mutation

--- src/one-time-password/one-time-password.resolver.ts
+++ src/one-time-password/one-time-password.resolver.ts
@@ -1,20 +1,27 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, registerEnumType, Resolver } from '@nestjs/graphql';
+import { OneTimePasswordType } from '@prisma/client';
 import { OneTimePasswordService } from './one-time-password.service';
+
+registerEnumType(OneTimePasswordType, { name: 'OneTimePasswordType' });
 
 @Resolver()
 export class OneTimePasswordResolver {
   constructor(private readonly otpService: OneTimePasswordService) {}
 
   @Mutation(() => Boolean)
-  sendPhoneOTP(@Args({ name: 'phone', type: () => String }) phone: string) {
-    this.otpService.sendPhoneOTP(phone);
-
-    return true;
-  }
-
-  @Mutation(() => Boolean)
-  sendEmailOTP(@Args({ name: 'email', type: () => String }) email: string) {
-    this.otpService.sendEmailOTP(email);
+  sendOTP(
+    @Args({ name: 'type', type: () => OneTimePasswordType })
+    type: OneTimePasswordType,
+    @Args({ name: 'value', type: () => String }) value: string,
+  ) {
+    switch (type) {
+      case OneTimePasswordType.EMAIL:
+        this.otpService.sendEmailOTP(value);
+        break;
+      case OneTimePasswordType.SMS:
+        this.otpService.sendPhoneOTP(value);
+        break;
+    }
 
     return true;
   }
