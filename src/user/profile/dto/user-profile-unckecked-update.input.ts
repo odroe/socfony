fb50231044f7a6a73f34c053482931c8de807b7d
Seven Du <whine.whitish.0u@icloud.com>
2022-04-10 16:52:18 +0800

    🐛 Fixed update user profile gender is required

--- src/user/profile/dto/user-profile-unckecked-update.input.ts
+++ src/user/profile/dto/user-profile-unckecked-update.input.ts
@@ -1,14 +1,13 @@
-import { InputType, PickType } from '@nestjs/graphql';
+import { InputType, PartialType, PickType } from '@nestjs/graphql';
 import { Prisma } from '@prisma/client';
 import { UserProfile } from '../entities/user-profile.entity';
 
 @InputType()
 export class UserProfileUncheckedUpdateInput
-  extends PickType(
+  extends PartialType(PickType(
     UserProfile,
     ['bio', 'birthday', 'gender'] as const,
-    InputType,
-  )
+  ), InputType)
   implements
     Pick<
       Prisma.UserProfileUncheckedUpdateInput,
