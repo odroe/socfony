cc1be28770ed971dd73f813d2ad3f8647b4589f3
Seven Du <whine.whitish.0u@icloud.com>
2022-07-04 12:22:00 +0800

    chore: Add phone sent code check method

--- server/lib/grpc_services/phone_one_time_password_service.dart
+++ server/lib/grpc_services/phone_one_time_password_service.dart
@@ -1,6 +1,8 @@
 import 'package:grpc/grpc.dart';
 import 'package:socfonyapis/socfonyapis.dart';
 
+import '../database/models/phone_sent_code_model.dart';
+import '../database/repositories/phone_sent_code_repository.dart';
 import '../services/access_token/short_message_service.dart';
 
 class PhoneOneTimePasswordService extends PhoneOneTimePasswordServiceBase {
@@ -13,8 +15,20 @@ class PhoneOneTimePasswordService extends PhoneOneTimePasswordServiceBase {
 
   @override
   Future<BoolValue> check(
-      ServiceCall call, CheckPhoneOneTimePasswordRequest request) {
-    // TODO: implement check
-    throw UnimplementedError();
+      ServiceCall call, CheckPhoneOneTimePasswordRequest request) async {
+    /// Get phone sent code repository.
+    const PhoneSentCodeRepository repository = PhoneSentCodeRepository();
+
+    /// Find a phone sent code.
+    final PhoneSentCodeModel? phoneSentCode =
+        await repository.find(request.phone);
+
+    /// Check if phone sent code is expired.
+    if (phoneSentCode == null ||
+        phoneSentCode.expiredAt.isBefore(DateTime.now())) {
+      return BoolValue(value: false);
+    }
+
+    return BoolValue(value: true);
   }
 }
