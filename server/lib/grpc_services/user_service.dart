b5de3b4540d7052327bad8855720c388074bdc73
Seven Du <whine.whitish.0u@icloud.com>
2022-07-05 18:44:08 +0800

    chore(server): Implement update user phone method

--- server/lib/grpc_services/user_service.dart
+++ server/lib/grpc_services/user_service.dart
@@ -2,7 +2,9 @@ import 'package:grpc/grpc.dart';
 import 'package:socfonyapis/socfonyapis.dart';
 
 import '../database/models/access_token_model.dart';
+import '../database/models/phone_sent_code_model.dart';
 import '../database/models/user_model.dart';
+import '../database/repositories/phone_sent_code_repository.dart';
 import '../database/repositories/user_repository.dart';
 import '../services/access_token/auth_service.dart';
 
@@ -45,9 +47,75 @@ class UserService extends UserServiceBase {
   }
 
   @override
-  Future<User> updatePhone(ServiceCall call, UpdateUserPhoneRequest request) {
-    // TODO: implement updatePhone
-    throw UnimplementedError();
+  Future<User> updatePhone(
+      ServiceCall call, UpdateUserPhoneRequest request) async {
+    /// Get required current authenticated access token.
+    final AccessTokenModel accessToken = await AuthService(call).required();
+
+    /// Create user repository.
+    const UserRepository userRepository = UserRepository();
+
+    /// Create Phone Sent code repository.
+    const PhoneSentCodeRepository phoneSentCodeRepository =
+        PhoneSentCodeRepository();
+
+    /// Get current authenticated user.
+    final UserModel user = await userRepository.find(accessToken.ownerId);
+
+    /// Find phone sent code by current authenticated user.
+    final PhoneSentCodeModel? before =
+        await phoneSentCodeRepository.find(user.phone);
+
+    /// Check if phone sent code is expired.
+    if (before == null || before.expiredAt.isBefore(DateTime.now())) {
+      throw GrpcError.invalidArgument(
+          'Phone sent code is expired. Please request new one.');
+    }
+
+    /// Check if phone sent code is correct.
+    if (before.code != request.otp) {
+      throw GrpcError.invalidArgument('Phone sent code is incorrect.');
+    }
+
+    /// Find phone sent code by new phone.
+    final PhoneSentCodeModel? after =
+        await phoneSentCodeRepository.find(request.new_2.phone);
+
+    /// Check if phone sent code is expired.
+    if (after == null || after.expiredAt.isBefore(DateTime.now())) {
+      throw GrpcError.invalidArgument(
+          'Phone sent code is expired. Please request new one.');
+    }
+
+    /// Check if phone sent code is correct.
+    if (after.code != request.new_2.otp) {
+      throw GrpcError.invalidArgument('Phone sent code is incorrect.');
+    }
+
+    /// Find user by new phone.
+    final UserModel? userAfter = await userRepository.findByPhone(
+      request.new_2.phone,
+    );
+
+    /// Delete all phone sent codes.
+    phoneSentCodeRepository
+      ..delete(before)
+      ..delete(after);
+
+    /// Check if user with new phone exists.
+    if (userAfter != null && userAfter.id != user.id) {
+      throw GrpcError.invalidArgument('User with this phone already exists.');
+
+      /// If new phone exists user same as current authenticated user.
+    } else if (userAfter != null && userAfter.id == user.id) {
+      return _maskUserPhone(user.toGrpcMessage(), mask: true);
+    }
+
+    /// Update user phone.
+    final UserModel updated =
+        await userRepository.updatePhone(user.id, request.new_2.phone);
+
+    return _maskUserPhone(updated.toGrpcMessage(), mask: true);
   }
 
   @override
@@ -63,7 +131,7 @@ class UserService extends UserServiceBase {
 
     /// Check if user exists and is not the same as current user.
     if (user != null && user.id != accessToken.ownerId) {
-      throw Exception('Username already exists.');
+      throw GrpcError.alreadyExists('Username already exists.');
 
       /// If user is the same as current user, return user.
     } else if (user != null && user.id == accessToken.ownerId) {
