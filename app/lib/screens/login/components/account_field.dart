18268cd43532119bd964dc09b41ab359f15dcc5e
Seven Du <whine.whitish.0u@icloud.com>
2022-07-16 11:25:22 +0800

    chore(app): WIP

--- app/lib/screens/login/components/account_field.dart
+++ app/lib/screens/login/components/account_field.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class LoginAccountField extends StatelessWidget {
   const LoginAccountField({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextField();
+    return const TextField();
   }
 }
