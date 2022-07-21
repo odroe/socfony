41c6b4b20773925ea3c37167bad58540eac16b23
Seven Du <whine.whitish.0u@icloud.com>
2022-07-21 15:03:10 +0800

    fix(server): Fixed refresh fail

--- server/lib/socfony_service_mixins/access_token.dart
+++ server/lib/socfony_service_mixins/access_token.dart
@@ -54,7 +54,7 @@ mixin AccessTokenMethods on SocfonyServiceBase {
 
     // Create access token by user id.
     final Map<String, dynamic> accessToken =
-        await _createAccessToken(connection, user['id']);
+        await _createAccessToken(connection.query, user['id']);
 
     // Delete phone sent code.
     await connection.execute(
@@ -88,14 +88,6 @@ mixin AccessTokenMethods on SocfonyServiceBase {
     final Map<String, dynamic> accessToken =
         await Auth(connection).refresh(call);
 
-    // Update current access token.
-    // - [expired_at] is set to now + 5 minutes.
-    // - [refresh_expired_at] is set to now.
-    await connection.execute(
-      'UPDATE access_tokens SET expired_at = NOW() + INTERVAL \'5 minutes\', refresh_expired_at = NOW() WHERE token = @token',
-      substitutionValues: {'token': accessToken['token']},
-    );
-
     final Map<String, dynamic> result = await connection
         .transaction((PostgreSQLExecutionContext connection) async {
       // Update current access token.
@@ -108,7 +100,7 @@ mixin AccessTokenMethods on SocfonyServiceBase {
 
       // Create token and return it.
       return await _createAccessToken(
-          connection as dynamic, accessToken['owner_id']);
+          connection.query, accessToken['owner_id']);
     });
 
     // Close database connection.
@@ -148,9 +140,16 @@ mixin AccessTokenMethods on SocfonyServiceBase {
   }
 
   Future<Map<String, dynamic>> _createAccessToken(
-      PooledDatabaseConnection connection, String userId) async {
+      Future<PostgreSQLResult> Function(
+    String sql, {
+    Map<String, dynamic>? substitutionValues,
+    bool? allowReuse,
+    int? timeoutInSeconds,
+  })
+          query,
+      String userId) async {
     // Create access token.
-    final PostgreSQLResult result = await connection.query(
+    final PostgreSQLResult result = await query(
       'INSERT INTO access_tokens (token, owner_id, created_at, expired_at, refresh_expired_at) VALUES (@token, @ownerId, NOW(), @expiredAt, @refreshExpiredAt) RETURNING *',
       substitutionValues: {
         'token': 128.betid,
