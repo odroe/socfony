7fd1dfd764a478b3f0d7ba322896feb3d2524c2a
Seven Du <whine.whitish.0u@icloud.com>
2022-07-26 22:22:12 +0800

    feat(server): add send & check OTP method for auth

--- server/lib/socfony_service_mixins/phone_otp.dart
+++ server/lib/socfony_service_mixins/phone_otp.dart
@@ -1,6 +1,8 @@
 import 'package:grpc/grpc.dart';
+import 'package:postgres/postgres.dart';
 import 'package:socfonyapis/socfonyapis.dart';
 
+import '../auth.dart';
 import '../database/connection.dart';
 import '../phone_ont_time_password/otp_sender_service.dart';
 
@@ -52,5 +54,82 @@ mixin PhoneOneTimePasswordMethods on SocfonyServiceBase {
     await connection.close();
 
     return BoolValue()..value = isValid;
+  }
+
+  /// Send a one-time password for current authenticated user.
+  @override
+  Future<Empty> sendPhoneOneTimePassword2auth(
+      ServiceCall call, Empty request) async {
+    // Create database connection.
+    final PooledDatabaseConnection connection =
+        await PooledDatabaseConnection.connect();
+
+    // Get current access token.
+    final Map<String, dynamic> accessToken =
+        await Auth(connection).required(call);
+
+    // Get current user data.
+    final PostgreSQLResult result = await connection.query(
+      'SELECT phone FROM users WHERE id = @id LIMIT 1',
+      substitutionValues: {'id': accessToken['owner_id']},
+    );
+
+    // Find user phone in item.
+    for (final PostgreSQLResultRow element in result) {
+      final String? phone = element.toColumnMap()['phone'];
+
+      // If phone is null or is empty, throw error.
+      if (phone == null || phone.isEmpty) {
+        throw GrpcError.invalidArgument('请先绑定手机号码');
+      }
+
+      // Send a one-time password to the phone.
+      await PhoneOtpSenderService(connection).send(phone);
+
+      // Close database connection.
+      await connection.close();
+    }
+
+    throw GrpcError.unknown('未知错误');
+  }
+
+  /// Check if the code is valid for current authenticated user.
+  @override
+  Future<BoolValue> checkPhoneOneTimePassword2auth(
+      ServiceCall call, StringValue request) async {
+    // Create database connection.
+    final PooledDatabaseConnection connection =
+        await PooledDatabaseConnection.connect();
+
+    // Get current access token.
+    final Map<String, dynamic> accessToken =
+        await Auth(connection).required(call);
+
+    // Get current user data.
+    final PostgreSQLResult result = await connection.query(
+      'SELECT phone FROM users WHERE id = @id LIMIT 1',
+      substitutionValues: {'id': accessToken['owner_id']},
+    );
+
+    // Find user phone in item.
+    for (final PostgreSQLResultRow element in result) {
+      final String? phone = element.toColumnMap()['phone'];
+
+      // If phone is null or is empty, throw error.
+      if (phone == null || phone.isEmpty) {
+        return BoolValue()..value = false;
+      }
+
+      // Check if the code is valid.
+      final bool isValid = await PhoneOtpSenderService(connection)
+          .validate(phone, request.value);
+
+      // Close database connection.
+      await connection.close();
+
+      return BoolValue()..value = isValid;
+    }
+
+    return BoolValue()..value = false;
   }
 }
